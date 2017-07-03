ba51048167e369f4702314ec0f74000cd9d9f141
Pablo Magaz <pmagaz@atsistemas.com>
2017-07-03 21:51:10 +0200

    Add tests

--- spec/createReduxReducer.spec.js
+++ spec/createReduxReducer.spec.js
@@ -1,65 +1,51 @@
-import { expect } from 'chai';
+import { expect, should } from 'chai';
 import { createReduxReducer } from '../src/createReduxReducer';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 
 
 describe('CreateReduxReducer', () => {
-  it('should return a right reducer function', () => {
-
-    const actionHandlers = { TEST: reducerFunction };
-    const expectedState = { id: 1 };
-    const expectedAction = { type: 'TEST' }; 
-    const reducer = createReduxReducer(actionHandlers);
-
-    reducer({ id: 1 }, { type: 'TEST' });
-
-    function reducerFunction(state, action) {
-      expect(state).deep.equal(expectedState);
-      expect(action).deep.equal(expectedAction);
-      return { ...state };
-    }
-
-  });
-
-  it('should provide a reducer initial state', () => {
-
-    const actionHandlers = { TEST: reducerFunction };
-    const expectedState = { initialState: true };
-    const expectedAction = { type: 'TEST', payload: { id: 22 } }; 
-    const reducer = createReduxReducer(actionHandlers, expectedState);    
-    const rootReducer = combineReducers({
-      reducer
-    });
-
-    const store = createStore(rootReducer, { });
-    store.dispatch(expectedAction);
+ 
+  it('should return a correct reduce function', () => {
 
     const reducerFunction = (state, action) => {
       expect(state).deep.equal(expectedState);
       expect(action).deep.equal(expectedAction);
       return { ...state };
-    }
+    };
+
+    const actionHandlers = { TEST: reducerFunction };
+    const expectedState = { initialState: true };
+    const expectedAction = { type: 'TEST', payload: { id: 22 } }; 
+    const reducer = createReduxReducer(actionHandlers, expectedState);    
+    
+    reducer(expectedState, expectedAction);
 
   });
 
-  it('should return state with a non function', () => {
+   it('should return state without action or state', () => {
+    const actionHandlers = { TEST: reducerFunction };
+    const expectedState = { id: 1 };
+    const expectedAction = { type: 'TEST' }; 
+    const reducer = createReduxReducer(actionHandlers);
 
-    const actionHandlers = { TEST: reducerFunction, TEST_2: 22 };
-    const expectedState = { initialState: true };
-    const expectedAction = { type: 'TEST_2', payload: { id: 22 } }; 
-    
-    const reducer = createReduxReducer(actionHandlers, expectedState); 
-    const rootReducer = combineReducers({
-      reducer
-    }); 
-    const store = createStore(rootReducer, { });
-    store.dispatch(expectedAction);
+    reducer();
 
     function reducerFunction(state, action) {
       expect(state).deep.equal(expectedState);
       expect(action).deep.equal(expectedAction);
       return { ...state };
     }
+
+  });
+
+  it('should throw Error with non function parameter', (done) => {
+    const actionHandlers = { TEST: 22};
+    const expectedState = { initialState: true };
+    const expectedAction = { type: 'TEST', payload: { id: 22 } }; 
+    const reducer = createReduxReducer(actionHandlers, expectedState);    
+    
+    expect(() => reducer(expectedState, expectedAction)).to.throw(Error);
+    done();
 
   });
 });
